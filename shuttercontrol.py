1fdeaeef8fc5ce417bc97d899ddeb2324025d467
Max Aigner <Max Aigner>
2017-02-10 19:12:39 +0100

    added basic logic

--- shuttercontrol.py
+++ shuttercontrol.py
@@ -1,9 +1,23 @@
-
+import time 
+import RPi.GPIO as GPIO
 import datetime
 from Sun import Sun
+#RPI.GPIO Layout verwenden (wie Pin-Nummern)
+GPIO.setmode(GPIO.BOARD)
 
+PIN_SWITCH_UP = 7
+PIN_SWITCH_DOWN = 29
+# Taster Rauf und Runter
+GPIO.setup(7,GPIO.IN) # pin 7 -> Rauf 
+GPIO.setup(29,GPIO.IN)# Pin 29 zum runterfahren 
+# Relais Schaltungen
+GPIO.setup(3 , GPIO.OUT) # pin 3 -> rauf
+GPIO.setup(5 , GPIO.OUT) # pin 5 -> runter
+
+
+
+#Use GPS Coordinats for morning open and evening close
 coords = {'longitude' : 11.581981, 'latitude' : 48.135125 }
-
 sun = Sun()
 
 # print current time
@@ -21,3 +35,14 @@ hours = int(time)
 minutes = (time*60) % 60
 seconds = (time*3600) % 60
 print("%d:%02d.%02d" % (hours, minutes, seconds))
+
+
+#http://raspberrypiguide.de/howtos/raspberry-pi-gpio-how-to/# Dauerschleife
+while 1:
+    #prüfen ob Sonnenaufgang ist
+    #prüfen ob Sonnenuntergang ist
+    #prüfen ob RAUF gedrückt ist
+    #prüfen ob RUNTER gedrückt ist 
+
+    #warten
+    time.sleep(0.1)
