0965b3bb85c9ea94e9385a2c3130813abf7e8bec
Max Aigner <Max Aigner>
2017-02-18 14:07:20 +0100

    updated class new version

---
+++ index.py
@@ -0,0 +1,16 @@
+
+
+class Shutter(object):
+    """Shutter infront of the window controlling the hardware"""
+    def __init__(self):
+        print "[shutter] created"
+    def rise(self):
+        """starts moving the shutter upwards until stop is called"""
+        print "[shutter] start go up"
+    def lower(self):
+        """starts moving the shutter downwards until stop is called"""
+        print "[shutter] start go down"
+    def stop(self):
+        """stopps the shutter immediately"""
+        print "[shutter] stop"
+    
