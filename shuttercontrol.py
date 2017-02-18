a62ffd3f4c617f9357f275dd8aec6448100fdd7e
ma68546 <max_aigneraigner@web.de>
2017-02-18 01:47:06 +0000

    still wip

--- shuttercontrol.py
+++ shuttercontrol.py
@@ -8,8 +8,10 @@ from Sun import Sun
 
 def now ():
     mynow = datetime.datetime.utcnow().time()
-    return mynow.hour + mynow.minute/60 + mynow.second/3600
+    return 0.0 + mynow.hour + mynow.minute/60 + mynow.second/3600
 
+RELAISOFF = True #yes it is that mixed up with my relais..
+RELAISON = False
 PIN_SWITCH_UP = 7
 PIN_SWITCH_DOWN = 29
 PIN_RELAIS_UP = 3
@@ -21,6 +23,8 @@ GPIO.setup(PIN_SWITCH_DOWN,GPIO.IN)# Pin 29 zum runterfahren
 # Relais Schaltungen
 GPIO.setup(PIN_RELAIS_UP, GPIO.OUT) # pin 3 -> rauf
 GPIO.setup(PIN_RELAIS_DOWN, GPIO.OUT) # pin 5 -> runter
+GPIO.output(PIN_RELAIS_UP, False)
+GPIO.output(PIN_RELAIS_DOWN , False)
 
 """the always running Loop - controls sunset sunrise and buttons"""
 #Use GPS Coordinats for morning open and evening close
@@ -35,8 +39,13 @@ buttonPressedDown = False
 buttonsLocked = False
 alterStatusHochfahren = False
 alterStatusRunterfahren = False
-StartzeitBewegung = 0
+alterStatusStop = False
+StartzeitBewegung = 0.0
+StartzeitSwitchUp = 0.0
+StartzeitSwitchDown = 0.0
 stop = False
+mynow = 0.0
+
 #property GanzHochFahren -> setter -> if oldval == false && newval == true -> HochfahrenZeit = DateTime.Now
 #http://raspberrypiguide.de/howtos/raspberry-pi-gpio-how-to/# Dauerschleife 
 
@@ -44,7 +53,7 @@ while 1:
     time.sleep(0.1)
 
     # Sonnenstand Abfragen 
-    now =  now()
+    mynow =  now()
     sunrise = SUN.getSunriseTime(COORDS)['decimal']
     sunset = SUN.getSunsetTime(COORDS)['decimal']
     if sunrise < 6.5 - 1:
@@ -52,62 +61,74 @@ while 1:
     if sunset > 22 - 1:
         sunset = 22 - 1
 
-    if now > sunrise:
-        if now < sunrise + 1/60:
+    if mynow > sunrise:
+        if mynow < sunrise + 1/60:
             if datetime.datetime.now().day != lastUpDay:
                 lastUpDay = datetime.datetime.now().day
                 hochfahren = True
-    if now > sunset:
-        if now < sunset + 1/60:
+		print "Sunrise detected"
+    if mynow > sunset:
+        if mynow < sunset + 1/60:
             if datetime.datetime.now().day != lastDownDay:
                 lastDownDay = datetime.datetime.now().day
                 runterfahren = True
+		print "Sunset detected"
 
     # Taster Abfragen 
     if GPIO.input(PIN_SWITCH_UP):
+	print "up switch pressed"
         if not buttonsLocked:
             if not buttonPressedUp:
                 buttonPressedUp = True
                 StartzeitSwitchUp = now()
             else:
                 hochfahren = True
+		print "up switch -> Hochfahren Init"
     else:
-        buttonPressedUp = False
-        if buttonPressedUp and now - StartzeitSwitchUp > 1/60/60 * 20:
+	print "up switch released. timedif: " + str( mynow - StartzeitSwitchUp)
+        if buttonPressedUp and mynow - StartzeitSwitchUp < 1/60/60 * 20:
+	    print "up switch Stop"
             stop = True # stoppt sofort beim Loslassen, wenn Knopf nur kurz gedrueckt wurde.
+	buttonPressedUp = False
 
     if GPIO.input(PIN_SWITCH_DOWN):
+	print "down switch pressed"
         if not buttonsLocked:
             if not buttonPressedDown:
                 buttonPressedDown = True
                 StartzeitSwitchDown = now()
             else:
                 hochfahren = True
+		print "down switch --> Runterfahren Init"
     else:
-        buttonPressedDown = False
-        if buttonPressedDown and now - StartzeitSwitchDown > 1/60/60 * 20:
+	print "down switch released -> timedif: " + str(mynow - StartzeitSwitchDown)
+        if buttonPressedDown and mynow - StartzeitSwitchDown < 1/60/60 * 20:
+	    print "down switch Stop"
             stop = True # stoppt sofort beim Loslassen, wenn Knopf nur kurz gedrueckt wurde.
+	buttonPressedDown = False
 
     if not buttonPressedUp and not buttonPressedDown:
         buttonsLocked = False
 
     # Relais setzen
     if hochfahren:
+	print "Go Up"
         if not alterStatusHochfahren:
             alterStatusHochfahren = True
             StartzeitBewegung = now()
         runterfahren = False
-        GPIO.output(PIN_RELAIS_DOWN, GPIO.LOW)
+        GPIO.output(PIN_RELAIS_DOWN, RELAISOFF)
         time.sleep(0.2)
-        GPIO.output(PIN_RELAIS_UP, GPIO.HIGH)
+        GPIO.output(PIN_RELAIS_UP, RELAISON)
     elif runterfahren:
+	print "Go Down"
         if not alterStatusRunterfahren:
             alterStatusRunterfahren = True
             StartzeitBewegung = now()
         hochfahren = False
-        GPIO.output(PIN_RELAIS_UP, GPIO.LOW)
+        GPIO.output(PIN_RELAIS_UP, RELAISOFF)
         time.sleep(0.2)
-        GPIO.output(PIN_RELAIS_DOWN, GPIO.HIGH)
+        GPIO.output(PIN_RELAIS_DOWN, RELAISON)
 
     if not hochfahren:
         alterStatusHochfahren = False
@@ -115,12 +136,17 @@ while 1:
         alterStatusRunterfahren = False
 
     if stop:
+	if alterStatusStop == False:
+	    alterStatusStop = True
+    	    print "stop"
         stop = False
-        GPIO.output(PIN_RELAIS_DOWN, GPIO.LOW)
-        GPIO.output(PIN_RELAIS_UP, GPIO.LOW)
+        GPIO.output(PIN_RELAIS_DOWN, RELAISOFF)
+        GPIO.output(PIN_RELAIS_UP, RELAISOFF)
         hochfahren = False
         runterfahren = False
         buttonsLocked = True
+    else:
+        alterStatusStop = False
 
-    if now - StartzeitBewegung > 1/60 /60 *20: # nach 20 Sekunden schaltet sich rauf/runter selber ab.
+    if mynow - StartzeitBewegung > 1/60 /60 *20: # nach 20 Sekunden schaltet sich rauf/runter selber ab.
         stop = True
